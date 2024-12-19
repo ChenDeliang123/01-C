ecc765491618b90bd375937b0ad7178153468ea2
chen_dl <19967657938@163.com>
2024-12-19 22:30:32 +0800

    chap6

---
+++ chap2/4.test1.c
@@ -0,0 +1,15 @@
+/*
+请使用 printf 函数，求解⼀个数字 n 的⼗进制表示的数字位数
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    printf("please input number: ");
+    scanf("%d", &n);
+    int num = printf("%d\n", n) - 1;
+    printf("%d has %d digit(s)\n", n, num);
+    return 0;
+}
